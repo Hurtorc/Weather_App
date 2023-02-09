314da0242ad252594c41455d1d72fcf866eadf8f
Hurtorc <anire88@gmail.com>
2023-02-09 07:57:41 -0800

    completley reworked JS and got it working

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,25 @@
-var input = document.querySelector("#name_field");
+const submit = document.querySelector("#submit");
+const APIkey = "24ce9fa82a265157be2662144b0cf306";
 
-input.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    createWeatherdisplay(event.target.value);
-  }
-});
-
-var APIkey = "24ce9fa82a265157be2662144b0cf306";
-
-function getGeoLocate(query) {
-  fetch(
-    "http://api.openweathermap.org/geo/1.0/direct?q=${query},&limit=${limit}&appid=${APIkey}"
-      .then((response) => response.json())
-      .then((data) => {
-        console.log = JSON.stringify(data);
-      })
-  );
-}
-
-function getWeather({ lat, lon, units }) {
-  return fetch(
-    "http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}"
-  );
-}
-
-function createWeatherdisplay(location) {
-  return getGeoLocate(location)
+//create a function that on a click of the submit button, it will get the value of the input field and use it to make a request to the API
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+  const city = document.querySelector("#city").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIkey}`;
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      getWeather({ lat: data[0].lon, lon: data[0].lat })
-        .then((weatherResponse) => weatherResponse.json())
-        .then((weatherData) => {
-          var weatherIcon = document.createElement("img");
-          weatherIcon.src =
-            "http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png";
-          var currentWeather = document.createElement("p");
-          currentWeather.textContent =
-            "${weatherData.weather[0].main}: ${weatherData.weather[0].description}";
-          document.body.textContent = JSON.stringify(weatherData);
-          document.body.appendChild(weatherIcon);
-          document.body.appendChild(currentWeather);
-        })
-        .catch((error) => {
-          document.body.textContent = "Error: " + error;
-        });
+      console.log(data);
+      const temp = data.main.temp;
+      const weather = data.weather.main;
+      const icon = data.weather.id;
+      const humidity = data.main.humidity;
+      const windSpeed = data.wind.speed;
+      const tempEl = document.querySelector("#temp");
+      const humidityEl = document.querySelector("#humidity");
+      const windSpeedEl = document.querySelector("#windSpeed");
+      tempEl.textContent = temp;
+      humidityEl.textContent = humidity;
+      windSpeedEl.textContent = windSpeed;
     });
-}
+});
